b9683060c584737010a49b3ef691adbe07e155b2
Nishar <mdnishar75640@gmail.com>
2026-07-28 23:12:26 +0530

    Add files via upload

---
+++ 05_functions/01_solution.py
@@ -0,0 +1,14 @@
+# 1. Basic Function Syntax
+# Problem: Write a function to calculate and return the square of a number.
+
+# def square(number):
+#     print(number ** 2)
+
+# square(4)
+
+def square(number):
+    return number ** 2
+
+
+result = square(7)
+print(result)
